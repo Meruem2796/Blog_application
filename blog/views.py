06754e63884b2b6c21febbb6eab8630aa8171370
Abdou_Mourid <abdoumouhid96@yahoo.com>
2024-04-12 16:56:49 +0200

    Adding pagination to the post list view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
 from django.http import Http404
 
 # Create your views here.
@@ -7,6 +8,11 @@ from django.http import Http404
 
 def post_list(request):
     posts = Post.published.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+    
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
