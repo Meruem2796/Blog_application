a646250fd4e93f6762bf175aae7bb46583b2cd63
Abdou_Mourid <abdoumouhid96@yahoo.com>
2024-04-12 17:41:10 +0200

    Creating a pagination template

--- blog/views.py
+++ blog/views.py
@@ -7,12 +7,12 @@ from django.http import Http404
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     posts = paginator.page(page_number)
-    
+
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
