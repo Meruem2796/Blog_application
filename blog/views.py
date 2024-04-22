cc41331c61413f762d62da516985e4c678bd0fec
Abdou_Mourid <abdoumouhid96@yahoo.com>
2024-04-22 14:57:01 +0200

    Handling ModelForms in views

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 from django.http import Http404
 
 # Create your views here.
@@ -75,3 +76,22 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                                         'form': form,
                                                                         'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                                {'post': post,
+                                        'form': form,
+                                        'comment': comment})
