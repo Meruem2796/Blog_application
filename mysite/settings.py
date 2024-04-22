c7fbc9ca47ab7f1bdb185b15bf08649dcfbe97fa
Abdou_Mourid <abdoumouhid96@yahoo.com>
2024-04-22 14:28:24 +0200

    Creating a comment system: creating a model for comments

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,6 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email server configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'elmouridos@gmail.com'
-EMAIL_HOST_PASSWORD = 'auhu ktwv jipb kzsm'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
